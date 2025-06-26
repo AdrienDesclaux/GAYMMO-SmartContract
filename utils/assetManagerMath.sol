bf64be8acbe9a5ee1f44f8f552aa5ab6c8e04df6
Yohan <yohan.georgelin@gmail.com>
2025-06-27 00:19:54 +0200

    start to code maths function to calculate claiming rent functionnality. Add function to calculate rentability per seconds.

--- utils/assetManagerMath.sol
+++ utils/assetManagerMath.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.8.28;
 library AssetManagerMath {
     // Function to calculate the value of an asset based on its price and quantity
     // Adjusts for price feed decimals
-    function calculateRentPrice(uint256 rentPerMonth, uint256 months) internal pure returns (uint256) {
-        if (months == 0) {
+    function calculateAnnualRentPrice(uint256 rentPerMonth) internal pure returns (uint256) {
+        return   12 * rentPerMonth * 10 ** 18;
+    }
+
+    function calculateSecondsRentPrice(uint256 rentPerMonth) internal pure returns (uint256) {
+        if (rentPerMonth == 0) {
             return 0;
         }
-        return months * rentPerMonth * 10 ** 18;
+        uint256 annualRent = calculateAnnualRentPrice(rentPerMonth);
+        uint256 secondsInYear = calculateSecondsInYear();
+        return (annualRent) /  secondsInYear;
     }
 
     function calculateAssetValue(uint256 pricePerUnit, uint256 quantity) internal pure returns (uint256) {
@@ -17,4 +23,8 @@ library AssetManagerMath {
         }
         return pricePerUnit * quantity;
     } 
+
+    function calculateSecondsInYear() internal pure returns (uint256) {
+        return 365 days + 6 hours;
+    }
 }
